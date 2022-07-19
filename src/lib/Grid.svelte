7dea3f421018f6d55f1a8d6dc77a6324742cd821
Mike Johansen <mike_johansen101@yahoo.com>
2022-07-19 13:35:35 +0200

    Greedy Dikjstra works

--- src/lib/Grid.svelte
+++ src/lib/Grid.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
-  let START_NODE_ROW = 1;
-  let START_NODE_COL = 3;
-  let FINISH_NODE_ROW = 10;
-  let FINISH_NODE_COL = 7;
+  let START_NODE_ROW = 10;
+  let START_NODE_COL = 10;
+  let FINISH_NODE_ROW = 25;
+  let FINISH_NODE_COL = 25;
   let grid = [40, 40];
   $: MAX_col = `repeat(${grid[1]}, 1fr)`;
   $: MAX_row = `repeat(${grid[0]}, 1fr)`;
-  let nodes = [];
+  let nodeGrid = [];
   let currentNode = null;
-  let currentPaths = [];
-  let previousNode = null;
 
   //Setup 2D grid array
   for (let i = 0; i < grid[0]; i++) {
     let currentRow = [];
     for (let j = 0; j < grid[1]; j++) {
-      currentNode = createNode(j, i);
+      currentNode = createNode(i, j);
       currentRow.push(currentNode);
     }
-    nodes.push(currentRow);
+    nodeGrid.push(currentRow);
   }
 
   function createNode(row: number, col: number) {
-    console.log(nodes);
+    console.log(nodeGrid);
     return {
       row: row,
       col: col,
@@ -38,7 +36,11 @@
   function startSearch(Algorithm) {
     switch (Algorithm) {
       case "dijkstra":
-        startDijkstra();
+        const visitedNodesInOrder = startDijkstra();
+        const nodesInShortestPathOrder = getNodesInShortestPathOrder(
+          nodeGrid[FINISH_NODE_COL][FINISH_NODE_ROW]
+        );
+        animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
         break;
 
       default:
@@ -47,58 +49,127 @@
   }
 
   function startDijkstra() {
-    currentNode = nodes[START_NODE_COL][START_NODE_ROW];
-    currentNode.distance = 0;
+    const visitedNodesInOrder = [];
+    nodeGrid[START_NODE_COL][START_NODE_ROW].distance = 0;
+    console.log(
+      "Start node distance: " +
+        nodeGrid[START_NODE_COL][START_NODE_ROW].distance
+    );
+    //currentNode.distance = 0;
+    const unvisitedNodes = getAllNodes(nodeGrid);
 
-    var interval = setInterval(() => {
-      addVisitedClass();
-      console.log("Current node: " + currentNode.col + " " + currentNode.row);
-      currentPaths = [];
-      addPaths();
-      previousNode = currentNode;
-      currentNode = currentPaths.pop();
-      currentNode.previousNode = previousNode;
-      if (currentNode !== undefined) {
-        if (currentNode.isFinish) {
-          console.log("Clear interval");
-          clearInterval(interval);
-          return;
-        }
+    while (!!unvisitedNodes.length) {
+      console.log("Started traverse grid...");
+      sortNodesByDistance(unvisitedNodes);
+      const closestNode = unvisitedNodes.shift();
+      console.log("Distance of current node: " + closestNode.distance);
+      // If we encounter a wall, we skip it.
+      if (closestNode.isWall) continue;
+      // If the closest node is at a distance of infinity,
+      // we must be trapped and should therefore stop.
+      if (closestNode.distance === Infinity) return visitedNodesInOrder;
+      closestNode.isVisited = true;
+      //addVisitedClass(closestNode);
+      visitedNodesInOrder.push(closestNode);
+      if (closestNode === nodeGrid[FINISH_NODE_COL][FINISH_NODE_ROW]) {
+        console.log("Found finish node");
+        return visitedNodesInOrder;
       }
-      console.log("iteration done");
-    }, 25);
+      updateUnvisitedNeighbors(closestNode, nodeGrid);
+    }
   }
 
-  function addVisitedClass() {
-    currentNode.isVisited = true;
-    let element = document.getElementById(
-      currentNode.col + "_" + currentNode.row
-    );
+  function sortNodesByDistance(unvisitedNodes) {
+    unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+  }
+
+  function addVisitedClass(node) {
+    let element = document.getElementById(node.col + "_" + node.row);
     element.classList.add("visited");
   }
 
-  function addPaths() {
-    //Up
-    let up = nodes[currentNode.col + 1][currentNode.row];
-    if (up !== undefined && up.isVisited == false) {
-      currentPaths.push(up);
+  function addShortestNodeClass(node) {
+    let element = document.getElementById(node.col + "_" + node.row);
+    element.classList.add("shortest");
+  }
+
+  function updateUnvisitedNeighbors(node, grid) {
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+    for (const neighbor of unvisitedNeighbors) {
+      neighbor.distance = node.distance + 1;
+      neighbor.previousNode = node;
     }
-    //Down
-    let down = nodes[currentNode.col - 1][currentNode.row];
-    if (down !== undefined && down.isVisited == false) {
-      currentPaths.push(down);
+  }
+
+  function getUnvisitedNeighbors(node, grid) {
+    const neighbors = [];
+    const { col, row } = node;
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+    return neighbors.filter((neighbor) => !neighbor.isVisited);
+  }
+
+  function getAllNodes(grid) {
+    const nodes = [];
+    for (const row of grid) {
+      for (const node of row) {
+        nodes.push(node);
+      }
     }
-    //Right
-    let right = nodes[currentNode.col][currentNode.row + 1];
-    if (right !== undefined && right.isVisited == false) {
-      currentPaths.push(right);
+    return nodes;
+  }
+
+  function getNodesInShortestPathOrder(finishNode) {
+    console.log("Started to find shortest path");
+
+    const nodesInShortestPathOrder = [];
+    let currentNode = finishNode;
+    console.log(
+      "previous node: " +
+        currentNode.previousNode.col +
+        " ---- " +
+        currentNode.previousNode.row
+    );
+    while (currentNode !== null) {
+      console.log(
+        "Current node coords: " + currentNode.col + " -- " + currentNode.row
+      );
+      nodesInShortestPathOrder.unshift(currentNode);
+      currentNode = currentNode.previousNode;
     }
-    //Left
-    let left = nodes[currentNode.col][currentNode.row - 1];
-    if (left !== undefined && left.isVisited == false) {
-      currentPaths.push(left);
+    return nodesInShortestPathOrder;
+  }
+
+  // ANIMATION
+  function animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    console.log("Started to animate visited nodes");
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      //Animate shortest path nodes
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          animateShortestPath(nodesInShortestPathOrder);
+        }, 15 * i);
+        return;
+      }
+
+      //Animate visited nodes
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        console.log("Current node to animate: " + node);
+        if (node != undefined) addVisitedClass(node);
+      }, 15 * i);
     }
-    console.log("ey");
+  }
+
+  function animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        addShortestNodeClass(node);
+      }, 50 * i);
+    }
   }
 </script>
 
@@ -106,13 +177,14 @@
 <div class="grid">
   <div
     class="container"
-    style="grid-template-rows: {MAX_col} ; grid-template-columns: {MAX_row};"
+    style="grid-template-rows: {MAX_row} ; grid-template-columns: {MAX_col};"
   >
-    {#each nodes as currRow, i (i)}
+    {#each nodeGrid as currRow, i (i)}
       {#each currRow as currNode, j (j)}
         <div
           id="{i}_{j}"
-          class:visited={currNode.isVisited}
+          class:visited={false}
+          class:shortest={false}
           class:finish={currNode.isFinish}
           class:start={currNode.isStart}
         />
@@ -138,6 +210,10 @@
 
   .container div.visited {
     background: var(--clrVisited);
+  }
+
+  .container div.shortest {
+    background: rgba(255, 106, 136, 0.842);
   }
 
   .container div.finish {
